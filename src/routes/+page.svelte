4420cfdc6a725cc0deb04e58d9fc9b36c49d6330
robsyc <robbe.claeys@gmail.com>
2024-06-25 22:54:10 +0200

    hero-section finished and started working on sign-up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,51 +1,105 @@
-<!-- <h1 class="font-robomo text-primary-800 mb-4 text-4xl font-bold lg:text-5xl italic"
->Murof heropage!</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
-
-	
 <script lang="ts">
-    import { Button } from "$lib/components/ui/button";
-    // import * as Card from "$lib/components/ui/card";
-	  import * as Menubar from "$lib/components/ui/menubar";
-    import { toggleMode } from "mode-watcher";
+  import HeroSection from "$lib/components/custom/HeroSection.svelte";
+  import { Button } from "$lib/components/ui/button";
+  import * as Card from "$lib/components/ui/card";
+  import * as Menubar from "$lib/components/ui/menubar";
+  import { toggleMode } from "mode-watcher";
+  import Sun from "lucide-svelte/icons/sun";
+  import Moon from "lucide-svelte/icons/moon";
 
-    import Sun from "lucide-svelte/icons/sun";
-    import Moon from "lucide-svelte/icons/moon";
+  const year = new Date().getFullYear();
 </script>
- 
+
 <header class="sticky top-0 z-50 w-full p-3 border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
   <Menubar.Root class="border-none bg-transparent">
-    <div class="container flex max-w-6xl items-center p-1 md:p-8 lg:p-12">
-    <Menubar.Menu>
+    <div class="container flex max-w-9xl items-center p-1">
+      <Menubar.Menu>
 
-      <div class="flex float-right justify-end w-full">
-      <Menubar.Item>
-        <Button on:click={toggleMode} variant="outline">
-            <Sun
-              class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-            />
-            <Moon
-              class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-            />
-            <span class="sr-only">Toggle theme</span>
-        </Button>
-      </Menubar.Item>
-      </div>
+        <Menubar.Item class="hidden text-foreground/70 transition-colors hover:text-foreground/85 lg:block">
+          <a href="#features">Features</a>
+        </Menubar.Item>
+        <Menubar.Item class="hidden text-foreground/70 transition-colors hover:text-foreground/85 lg:block">
+          <a href="#pricing">Pricing</a>
+        </Menubar.Item>
 
-      <Menubar.Trigger class="p-0">
-          <Button variant="default">
-            Start now
-          </Button>
-      </Menubar.Trigger>
-      <Menubar.Content>
-        <Menubar.Item>Learn more</Menubar.Item>
-        <Menubar.Separator />
-        <Menubar.Item>Sign up</Menubar.Item>
-        <Menubar.Item>Login</Menubar.Item>
-      </Menubar.Content>
+        <div class="flex float-right justify-end w-full">
+          <Menubar.Item>
+            <Button on:click={toggleMode} variant="outline">
+              <Sun class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"/>
+              <Moon class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
+              <span class="sr-only">Toggle theme</span>
+            </Button>
+          </Menubar.Item>
+        </div>
 
-    </Menubar.Menu>
+        <Menubar.Trigger class="p-0">
+          <Button variant="default">Start now</Button>
+        </Menubar.Trigger>
+        <Menubar.Content>
+          <Menubar.Item><a href="/test">Learn more</a></Menubar.Item>
+          <Menubar.Separator />
+          <Menubar.Item><a href="/auth/signup">Sign up</a></Menubar.Item>
+          <Menubar.Item><a href="/auth/login">Login</a></Menubar.Item>
+        </Menubar.Content>
+
+      </Menubar.Menu>
     </div>
   </Menubar.Root>
 </header>
 
+<HeroSection />
+
+<!-- Features section -->
+<section id="features" class="container mt-16">
+  <h2 class="font-robomo text-4xl font-bold mb-8">Features</h2>
+  <div class="grid grid-cols-1 gap-8 md:grid-cols-2 pb-8">
+    <div><img src="https://placeimg.com/500/300/arch" alt="feature 1" /></div>
+    <Card.Root class="bg-base-100">
+      <Card.Header>
+        <Card.Title>Feature 1</Card.Title>
+        <Card.Description>Lorem ipsum dolor sit amet consectetur adipisicing elit.</Card.Description>
+      </Card.Header>
+      <Card.Content>Lorem ipsum dolor sit amet consectetur adipisicing elit.</Card.Content>
+      <Card.Footer><Button>Get started</Button></Card.Footer>
+    </Card.Root>
+  </div>
+  <div class="grid grid-cols-1 gap-8 md:grid-cols-2 pb-9">
+    <div><img src="https://placeimg.com/500/300/arch" alt="feature 2" /></div>
+    <Card.Root class="bg-base-100">
+      <Card.Header>
+        <Card.Title>Feature 2</Card.Title>
+        <Card.Description>Lorem ipsum dolor sit amet consectetur adipisicing elit.</Card.Description>
+      </Card.Header>
+    </Card.Root>
+  </div>
+</section>
+
+<!-- Pricing section -->
+<section id="pricing" class="container mt-16">
+  <h2 class="font-robomo text-4xl font-bold mb-8">Pricing</h2>
+  <div class="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+    <Card.Root class="bg-base-100">
+      <Card.Header>
+        <Card.Title>Pricing 1</Card.Title>
+        <Card.Description>Lorem ipsum dolor sit amet consectetur adipisicing elit.</Card.Description>
+      </Card.Header>
+    </Card.Root>
+    <Card.Root class="bg-base-100">
+      <Card.Header>
+        <Card.Title>Pricing 2</Card.Title>
+        <Card.Description>Lorem ipsum dolor sit amet consectetur adipisicing elit.</Card.Description>
+      </Card.Header>
+    </Card.Root>
+    <Card.Root class="bg-base-100">
+      <Card.Header>
+        <Card.Title>Pricing 3</Card.Title>
+        <Card.Description>Lorem ipsum dolor sit amet consectetur adipisicing elit.</Card.Description>
+      </Card.Header>
+    </Card.Root>
+  </div>
+</section>
+
+<!-- Footer -->
+<footer class="container mt-16">
+  <p class="text-center">© {year} murof</p>
+</footer>
---
+++ src/routes/auth/signup/+page.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button"
+    import { Input } from "$lib/components/ui/input"
+    import { Label } from "$lib/components/ui/label"
+    import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter} from "$lib/components/ui/card"
+</script>
+
+
+<div class="flex flex-col items-center justify-center h-screen bg-base-100 lg:flex-row">
+    <div class="order-1 w-full lg:w-1/2">
+        <div class="flex justify-center items-start">
+            <!-- svelte-ignore a11y-img-redundant-alt -->
+            <img
+            src=""
+            alt="This is a placeholder image"
+            width="1920"
+            height="1080"
+            class="h-full object-cover dark:brightness-[0.2] dark:grayscale"
+            />
+        </div>
+    </div>
+
+    <div class="order-2 w-full lg:w-1/2">
+        <div class="flex justify-center items-start">
+        <Card class="p-4">
+            <CardHeader>
+                <CardTitle class="font-robomo text-3xl font-bold">Sign up</CardTitle>
+                <CardDescription class="text-balance text-muted-foreground">
+                    Enter your details to create your account
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <div class="grid gap-4">
+                    <div class="grid gap-2">
+                        <Label for="email">Email</Label>
+                        <Input
+                            id="email"
+                            type="email"
+                            placeholder="m@example.com"
+                            required
+                        />
+                    </div>
+                    <div class="grid gap-2">
+                        <div class="flex items-center">
+                            <Label for="password">Password</Label>
+                            <a
+                                href="/forgot-password"
+                                class="ml-auto inline-block text-sm underline"
+                            >
+                                Forgot your password?
+                            </a>
+                        </div>
+                        <Input id="password" type="password" required />
+                    </div>
+                    <Button class="w-full">
+                        Login
+                    </Button>
+                    <Button variant="outline" class="w-full">
+                        Login with Google
+                    </Button>
+                </div>
+            </CardContent>
+            <CardFooter>
+                <a href="/" class="underline">
+                    Don&apos;t have an account? Sign up
+                </a>
+            </CardFooter>
+        </Card>
+        </div>
+    </div>
+</div>
