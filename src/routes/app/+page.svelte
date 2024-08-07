3b0ec0df9bc075cd4624089628935b3e8ec0e791
robsyc <robbe.claeys@gmail.com>
2024-08-07 19:34:29 +0200

    first custom node

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -3,14 +3,20 @@
 
     import { SvelteFlow, Background, Controls } from '@xyflow/svelte';
     import '@xyflow/svelte/dist/style.css';
+
+    import Mu from './Mu.svelte';
+    const nodeTypes = {
+        'mu': Mu
+    };
     
     // export let data: PageData;
 
     const nodes = writable([
         {
         id: '1', // required and needs to be a string
+        type: 'mu',
         position: { x: 0, y: 0 }, // required
-        data: { label: 'hey' } // required
+        data: { color: writable('#ff4000') } // required
         },
         {
         id: '2',
@@ -24,7 +30,7 @@
 </script>
 
 <main>
-    <SvelteFlow {nodes} {edges} fitView>
+    <SvelteFlow {nodeTypes} {nodes} {edges} fitView>
         <!-- Tailwind variable colors don't work yet... -->
       <Background bgColor="--background" patternColor="accent-foreground" />
       <Controls showLock={false} />
