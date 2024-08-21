0d40f3ccfe962feb3fda0e4696dd4e23d39a3681
robsyc <robbe.claeys@gmail.com>
2024-08-21 02:00:35 +0200

    chore: Update npm dependencies to latest versions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
     import "../app.css";
     import { ModeWatcher } from "mode-watcher";
+    import { inject } from '@vercel/analytics'
+    import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+    injectSpeedInsights();
 </script>
    
 <ModeWatcher />
