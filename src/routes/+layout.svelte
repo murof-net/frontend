eedfb3aaec304d8296e452acb4e0690badd2ef11
robsyc <robbe.claeys@gmail.com>
2024-08-22 21:39:58 +0200

    refactor: Remove unused code in +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
     import "../app.css";
     import { ModeWatcher } from "mode-watcher";
-    import { inject } from '@vercel/analytics'
-    import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
-    injectSpeedInsights();
 </script>
    
 <ModeWatcher />
